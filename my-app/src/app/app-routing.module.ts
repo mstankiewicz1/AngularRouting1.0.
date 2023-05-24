a815c68d0998df0a512aba598f69f6ddae93a437
Marek <marekstankiewicz6@wp.pl>
2023-05-24 16:37:55 +0200

    added dynamic routing

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   },
   {
     component: UserComponent,
-    path: 'user'
+    path: 'user/:id'
   },
   {
     component: HomeComponent,
--- my-app/src/app/user/user.component.ts
+++ my-app/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+
+  userId: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    console.warn("user id is: " + this.route.snapshot.paramMap.get('id'));
+    this.userId = this.route.snapshot.paramMap.get('id');
+  }
 
 }
